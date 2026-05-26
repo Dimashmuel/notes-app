a0c3ca23003c3705264a50ed544b445906b68bc0
dimashmuel <dimashmuel12@gmail.com>
2026-05-26 14:35:49 +0300

    Implement delete note functionality

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,4 +1,4 @@
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, deleteNote }) => {
     if (notes.length === 0) {
         return <p className='text-center text-gray-500'>No notes yet</p>;
     }
@@ -6,9 +6,9 @@ const NoteList = ({ notes }) => {
         <div className='space-y-4'>
             {notes.map((note) => (
                 <div
-                 key={note.id}
-                 className='p-4 bg-white rounded-lg shadow-md border-l-4'
-                 >
+                    key={note.id}
+                    className='p-4 bg-white rounded-lg shadow-md border-l-4'
+                >
                     <h3 className='text-lg font-bold'>{note.title}</h3>
                     <p className='text-sm text-gray-600'>
                         <strong>Category: </strong> {note.category}
@@ -17,6 +17,10 @@ const NoteList = ({ notes }) => {
                         <strong>Priority: </strong> {note.priority}
                     </p>
                     <p className='mt-2'>{note.description}</p>
+                    <button onClick={() => deleteNote(note.id)} className='mt-3 text-red-500 cursor-pointer transition
+                     hover:text-red-700'>
+                        🗑 Delete
+                    </button>
                 </div>
             ))}
         </div>
