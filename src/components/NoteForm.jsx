1e076da3ec245c38cd51f1b3fe2fa58808a39a3c
dimashmuel <dimashmuel12@gmail.com>
2026-05-26 12:38:39 +0300

    Implement collapsible functionality for NoteForm

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -8,6 +8,8 @@ const NoteForm = ({ notes, setNotes }) => {
         category: 'Work'
     });
 
+    const [isFormVisible, setIsFormVisible] = useState(false);
+
     const handleChange = (e) => {
         setFormData({
             ...formData,
@@ -17,7 +19,7 @@ const NoteForm = ({ notes, setNotes }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        
+
         // Validation
         if (!formData.title || !formData.description) return;
 
@@ -37,68 +39,78 @@ const NoteForm = ({ notes, setNotes }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className='mb-6' >
-            <div className='mb-4'>
-                <label htmlFor='title' className='block font-semibold'>
-                    Title
-                </label>
-                <input
-                    name='title'
-                    type='text'
-                    className='w-full p-2 border rounded-lg'
-                    value={formData.title}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className='mb-4'>
-                <label htmlFor='priority' className='block font-semibold'>
-                    Priority
-                </label>
-                <select
-                    name='priority'
-                    type='text'
-                    className='w-full p-2 border rounded-lg'
-                    value={formData.priority}
-                    onChange={handleChange}
-                >
-                    <option value='High'>🔴 High</option>
-                    <option value='Medium'>🟠 Medium</option>
-                    <option value='Low'>🟢 Low</option>
-                </select>
-
-            </div>
-            <div className='mb-4'>
-                <label htmlFor='category' className='block font-semibold'>
-                    Category
-                </label>
-                <select
-                    name='category'
-                    type='text'
-                    className='w-full p-2 border rounded-lg'
-                    value={formData.category}
-                    onChange={handleChange}>
-                    <option value='Work'>📂 Work</option>
-                    <option value='Personal'>🏠 Personal</option>
-                    <option value='Ideas'>💡 Ideas</option>
-                </select>
-            </div>
-            <div className='mb-4'>
-                <label htmlFor='description' className='block font-semibold'>
-                    Description
-                </label>
-                <textarea
-                    name='description'
-                    type='text'
-                    className='w-full p-2 border rounded-lg'
-                    value={formData.description}
-                    onChange={handleChange}>
-                </textarea>
-            </div>
-            <button className='w-full bg-purple-500 text-white py-2
-             rounded-lg cursor-pointer hover:bg-purple-600'>
-                Add Note
+        <>
+            <button onClick={() => setIsFormVisible(!isFormVisible)}
+                className='w-full bg-gray-100 border border-gray-300 text-purple-800
+                    py-2 rounded-lg cursor-pointer hover:bg-purple-200
+                    hover:border-purple-300 mb-4'>
+                {isFormVisible ? 'Hide Form ✖️' : 'Add New Note ➕'}
             </button>
-        </form>
+            {isFormVisible && (
+                <form onSubmit={handleSubmit} className='mb-6' >
+                    <div className='mb-4'>
+                        <label htmlFor='title' className='block font-semibold'>
+                            Title
+                        </label>
+                        <input
+                            name='title'
+                            type='text'
+                            className='w-full p-2 border rounded-lg'
+                            value={formData.title}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='mb-4'>
+                        <label htmlFor='priority' className='block font-semibold'>
+                            Priority
+                        </label>
+                        <select
+                            name='priority'
+                            type='text'
+                            className='w-full p-2 border rounded-lg'
+                            value={formData.priority}
+                            onChange={handleChange}
+                        >
+                            <option value='High'>🔴 High</option>
+                            <option value='Medium'>🟠 Medium</option>
+                            <option value='Low'>🟢 Low</option>
+                        </select>
+
+                    </div>
+                    <div className='mb-4'>
+                        <label htmlFor='category' className='block font-semibold'>
+                            Category
+                        </label>
+                        <select
+                            name='category'
+                            type='text'
+                            className='w-full p-2 border rounded-lg'
+                            value={formData.category}
+                            onChange={handleChange}>
+                            <option value='Work'>📂 Work</option>
+                            <option value='Personal'>🏠 Personal</option>
+                            <option value='Ideas'>💡 Ideas</option>
+                        </select>
+                    </div>
+                    <div className='mb-4'>
+                        <label htmlFor='description' className='block font-semibold'>
+                            Description
+                        </label>
+                        <textarea
+                            name='description'
+                            type='text'
+                            className='w-full p-2 border rounded-lg'
+                            value={formData.description}
+                            onChange={handleChange}>
+                        </textarea>
+                    </div>
+                    <button className='w-full bg-purple-500 text-white py-2
+             rounded-lg cursor-pointer hover:bg-purple-600'>
+                        Add Note
+                    </button>
+                </form>
+            )}
+        </>
     );
 }
 
